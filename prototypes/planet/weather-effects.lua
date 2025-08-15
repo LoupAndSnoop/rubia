70588f18c77cc047ecc05f7d7eaf9fdbe30428e8
Loup <lnavarro@mylumatek.com>
2025-08-15 14:09:31 -0400

    Rubia wind particulates

--- prototypes/planet/weather-effects.lua
+++ prototypes/planet/weather-effects.lua
@@ -1,4 +1,4 @@
---Derrived from Celestial weather mod
+--Derrived from Celestial weather mod, by Varaxia
 
 rubia_effects = {}
 data.extend({
@@ -27,7 +27,6 @@ data.extend({
       filename = "__rubia-assets__/graphics/terrain/rubia-sand-particles.png",
       flags = { "smoke" }
     },
-    --vertical_speed_slowdown = 0.001
   },
 })
 
@@ -56,7 +55,65 @@ local cluster_particles = {
         }
     }
 }
+
+
+
+------ direct particles for rain-like lines------
+data.extend({
+{ --Based on aquilo snow smoke
+    type = "trivial-smoke",
+    name = "rubia-rain-lines",
+    duration = 200,
+    fade_in_duration = 30,
+    fade_away_duration = 30,
+    spread_duration= 60,
+    start_scale = 1,
+    end_scale = 1,
+    color = {0.582, 0.447, 0.378, 0.8},
+    cyclic = true,
+    affected_by_wind = true,
+    --movement_slow_down_factor = 0.0001,
+    animation =
+    {
+      width = 256,
+      height = 256,
+      line_length = 2,
+      frame_count = 4,
+      shift = {-0.53125, -0.4375},
+      priority = "high",
+      animation_speed = 0.0001,
+      scale = 1.3,
+      filename = "__rubia-assets__/graphics/terrain/rubia-side-rain.png",
+      flags = { "smoke" }
+    },
+  },
+})
+
+local direct_particles = {
+  type = "direct",
+  action_delivery = {
+    type = "instant",
+    source_effects = {
+      type = "create-trivial-smoke",
+      initial_height = 0.4,
+      speed_multiplier_deviation = 0.2,
+      starting_frame = 512,
+      starting_frame_deviation = 1024,
+      offset_deviation = {{-96, -48}, {96, 48}},
+      speed_from_center = 0,
+      speed_from_center_deviation = 0,
+
+      smoke_name = "rubia-rain-lines",
+      speed = {0.3, 0},
+      speed_multiplier = 2,
+      --probability = 0.5,
+    }
+  }
+}
+
+
 table.insert(rubia_effects, cluster_particles)
+table.insert(rubia_effects, direct_particles)
 
 assert(data.raw["planet"]["rubia"], "Rubia prototype is not defined yet, or has been removed!")
 data.raw["planet"]["rubia"].ticks_between_player_effects = 1
